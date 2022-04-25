cf0417c9d0a088c42bf9f3d7073fbb5112b3703c
alannxq <85573908+alannxq@users.noreply.github.com>
2022-04-25 23:33:18 +0100

    Add files via upload

---
+++ burtle/__init__.py
@@ -0,0 +1,7 @@
+from burtle.burtle import Burtle  # from burtle folder, from burtle file, import Burtle class
+from burtle.logic import mainloop, done
+from burtle.window import BScreen, window
+from burtle.key_binding import events
+from burtle.textbox import Textbox
+from burtle.gravity import gravity
+
