bab0afa265dac8abd75ca0651634af11f50dc0f5
alannxq <85573908+alannxq@users.noreply.github.com>
2022-04-25 23:30:37 +0100

    Delete __init__.py

--- burtle/__init__.py
+++
@@ -1,4 +0,0 @@
-from burtle.main import BScreen, mainloop, events, done, window
-from burtle.burtle import Burtle  # from burtle folder, from burtle file, import Burtle class
-from burtle.textbox import Textbox
-from burtle.gravity import gravity
