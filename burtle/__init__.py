a5566f6215886521e4c2137822ff0836b796b56c
alannxq <85573908+alannxq@users.noreply.github.com>
2022-05-03 23:57:23 +0100

    Update __init__.py

--- burtle/__init__.py
+++ burtle/__init__.py
@@ -1,7 +1,8 @@
 from burtle.burtle import Burtle  # from burtle folder, from burtle file, import Burtle class
-from burtle.logic import mainloop, done  # done should not be used, as only mainloop() or window.update() will update the screen
+from burtle.logic import mainloop, done, run
 from burtle.window import BScreen, window
-from burtle.key_binding import events
+from burtle.key_binding import key_events
 from burtle.textbox import Textbox
 from burtle.gravity import gravity
+from burtle.function_events import event
 
