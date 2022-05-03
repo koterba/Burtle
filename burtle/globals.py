171003ce8955be03ea0aced2100792926b69b3e1
alannxq <85573908+alannxq@users.noreply.github.com>
2022-05-04 00:00:16 +0100

    Update globals.py

--- burtle/globals.py
+++ burtle/globals.py
@@ -1,5 +1,7 @@
 affected_by_gravity = []
-movable_objects = []
-burtle_keybinds = []
-solid_objects = []
-all_burtles = []
+movable_objects = [] # a burtle that is not static, meaning that it will be affected by gravity by default
+burtle_keybinds = [] # all of the key binds that will be checked for after using the default_keys method on all burtles
+solid_objects = [] # objects that will be non-passable using the default_keys method on a burtle
+all_burtles = [] # every burtle object will be in this list
+
+function_events = [] # functions that will be ran in the mainloop after the user uses the decorator to add their function
