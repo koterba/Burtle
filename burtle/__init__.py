96416076553601ccecb7b42e7a36b762725bbbb1
alannxq <85573908+alannxq@users.noreply.github.com>
2022-06-21 18:51:27 +0100

    Update __init__.py

--- burtle/__init__.py
+++ burtle/__init__.py
@@ -1,9 +1,9 @@
-from burtle.burtle import Burtle  # from burtle folder, from burtle file, import Burtle class
-from burtle.logic import mainloop, done, run
-from burtle.window import BScreen, window
-from burtle.key_binding import key_events
-from burtle.textbox import Textbox
-from burtle.gravity import gravity
+from burtle.burtle          import Burtle  # from burtle folder, from burtle file, import Burtle class
+from burtle.logic           import mainloop, done, run
+from burtle.window          import BScreen, window
+from burtle.key_binding     import key_events
+from burtle.textbox         import Textbox
+from burtle.gravity         import gravity
 from burtle.function_events import event
 
 #
